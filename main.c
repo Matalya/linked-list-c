3e65224a5b6693442dd5f2ea7989a0ec21fc31a0
Matalya <matalya8@gmail.com>
2023-09-25 16:26:43 -0300

    ajjnmbashjfbfjsd

--- main.c
+++ main.c
@@ -3,36 +3,10 @@
 #include "numtypes.h"
 
 typedef struct {
-    int* prev;
-    int item;
-    int* next;
+    i64* prev;
+    i64 item;
+    i64* next;
 } eent;
 
-typedef struct {
-    int* head;
-    size_t size;
-} Array;
-
-int index_inbound(Array array, size_t index) {
-    return index >= array.size || index < 0;
-}
-
-Array createArray(size_t size) {
-    Array array = {
-        .head = malloc(sizeof(int) * size * 3),
-        .size = size,
-    };
-    array.head = 0;
-    array.head[-1] = 0;
-    array.head[1] = 0;
-    return array;
-}
-
-int appendItem(Array array, int item) {
-    
-    return 1;
-}
-
 int main() {
-    createArray(5);
 }
